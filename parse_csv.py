2661529bbd6fb9db72f72bd3e44c25b9ca7ca41f
SunnyCQ <sunny02px2022@gmail.com>
2024-08-17 19:21:35 -0400

    Created CSV with normal english words and parse_CSV now can read all lines

--- parse_csv.py
+++ parse_csv.py
@@ -1,1 +1,8 @@
 import csv
+
+#boilerplate code to open the csv. 'r' represents read
+with open('TestCSVEng.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for hi in csv_reader:
+        print(hi)
